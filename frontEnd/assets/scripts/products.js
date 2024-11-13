f99b96fe201ea8de944d5319de3c7facb21cc99a
Keltoummalouki <keltoummalouki911@gmail.com>
2024-11-13 16:18:53 +0100

    versionfinal

--- frontEnd/assets/scripts/products.js
+++ frontEnd/assets/scripts/products.js
@@ -1,63 +1,42 @@
+const produit = document.getElementById("products");
 const productContainer = document.getElementById('products');
-let products = [];
-let response;
-let products =[];
-const productContainer = document.getElementById('products');
-let response;
-async function fetchProducts() {
+let productsList = [];
+let addedProducts = JSON.parse(localStorage.getItem("addedProducts")) || [];
 
-    response = await fetch('http://localhost:5000/api/products');
-    products = await response.json();
-    displayProducts(products);
-    filterstock(products);
 
-        response = await fetch('http://localhost:5000/api/products');
+const fetchProducts = () => {
+    fetch("http://localhost:5000/api/products")
+        .then((response) => {
+            return response.json();
+        })
+        .then((products) => {
+            productsList = products;
+            displayProducts(products);
+        });
+};
 
-        products = await response.json(); 
-        //  console.log(products);
-        displayProducts(products);
-       return products;
+const add = (produitID) => {
+    const existedProduct = addedProducts.find(prd => prd.id == produitID)
+    if (!existedProduct) {
+        const firstProduct = productsList.find(prd => prd.id == produitID)
+        addedProducts.push({ ...firstProduct, quantity: 1 });
+    } else {
+        addedProducts = addedProducts.map(prd => {
+            if (prd.id == produitID) {
+                return { ...prd, quantity: prd.quantity + 1 }
+            }
+    
+            return prd;
+        })
+    }
+    localStorage.setItem('addedProducts',JSON.stringify(addedProducts))    
+     
 }
 
-// function displayProducts(products) {
-
-//     products.forEach(product => {
-//         const productCard = document.createElement('div');
-
-
-//         productCard.classList.add('flex', 'flex-col', 'items-center', 'space-y-4', 'bg-white', 'shadow-lg', 'm-3', 'p-3', 'w-auto');
-
-
-
-
-//         const imageUrl = product.image;
-
-
-//         productCard.innerHTML = `
-//             <img src="${imageUrl}" alt="${product.name}" class="h-24">
-//             <h3 class="text-xl font-semibold text-center">${product.name}</h3>
-//             <p class="text-gray-500 text-center">${product.category}</p>
-//             <p class="text-lg text-green-500 text-center">$${product.price.toFixed(2)}</p>
-            
-//             <div class="flex space-x-4">
-//                 <button style="background-color: #48bb78; color: white;" class="px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400">
-//                     Add to Cart
-//                 </button>
-//                 <button style="background-color: #4299e1; color: white;" class="px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-400">
-//                     Add to Favorites
-//                 </button>
-//             </div>
-//         `;
-
-
-//         productContainer.appendChild(productCard);
-//     });
-// }
-
- displayProducts = (products) => {
-    productContainer.innerHTML = ''
+const displayProducts = (products) => {
+    produit.innerHTML = ''
     products.forEach((product) => {
-        productContainer.innerHTML += `
+        produit.innerHTML += `
             <div
                 class="relative mt-10 ml-2 mr-4 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
                 <a class="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl" href="#">
@@ -74,75 +53,7 @@ async function fetchProducts() {
                     </a>
                     <div class="mt-2 mb-5 flex items-center justify-between">
                         <p>
-                            <span class="text-3xl font-bold text-slate-900">${product.prixNew}$</span>
-                            <span class="text-sm text-slate-900 line-through">${product.prix}</span>
-                        </p>
-                        <div class="flex items-center">
-                            <svg aria-hidden="true" class="h-5 w-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
-                                </path>
-                            </svg>
-                            <svg aria-hidden="true" class="h-5 w-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
-                                </path>
-                            </svg>
-                            <svg aria-hidden="true" class="h-5 w-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
-                                </path>
-                            </svg>
-                            <svg aria-hidden="true" class="h-5 w-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
-                                </path>
-                            </svg>
-                            <svg aria-hidden="true" class="h-5 w-5 text-gray-200" fill="currentColor" viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
-                                </path>
-                            </svg>
-                            <span class="mr-2 ml-3 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold">4.0</span>
-                        </div>
-                    </div>
-
-                    
-                    <a href="#"
-                        class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300 btnAdd" onclick="add(${product.id})">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24"
-                            stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                        </svg>
-                        Add to cart</a>
-                </div>
-function displayProducts (products)  {
-    productContainer.innerHTML = ''
-    products.forEach((product) => {
-        productContainer.innerHTML += `
-            <div
-                class="relative mt-10 ml-2 mr-4 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
-                <a class="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl" href="#">
-                    <div class="w-full h-full" style="background-color: #d6d5d1;">
-                        <img class="object-cover h-full w-full" src="${product.image}" alt="product image" />
-                    </div>
-                    <span class="absolute top-0 left-0 m-2 rounded-full bg-${product.sale === "Sale" ? "black" : "white"} px-2 text-center text-sm font-medium text-${product.sale === "Sale" ? "white" : "black"}">
-                        ${product.sale} <i class="fa-solid fa-fire-flame-curved"></i>
-                    </span>
-                </a>
-                <div class="mt-4 px-5 pb-5">
-                    <a href="#">
-                        <h5 class="text-xl tracking-tight text-slate-900">${product.name}</h5>
-                    </a>
-                    <div class="mt-2 mb-5 flex items-center justify-between">
-                        <p>
-                            <span class="text-3xl font-bold text-slate-900">${product.price}$</span>
+                            <span class="text-3xl font-bold text-slate-900">${product.prixNew}</span>
                             <span class="text-sm text-slate-900 line-through">${product.prix}</span>
                         </p>
                         <div class="flex items-center">
@@ -194,11 +105,31 @@ function displayProducts (products)  {
         `;
         
     });
+    compteur();
 };
 
+function search() {
+    const recherche = document.getElementById('search').value.toLowerCase();
+    const searchProducts = productsList.filter(prd => prd.nom_product.toLowerCase().includes(recherche));
+    displayProducts(searchProducts);
+}
 
+window.addEventListener("DOMContentLoaded", (e) => {
+    e.preventDefault();
+    fetchProducts();
+});
+
+function filterInStock() { 
+    let filter = productsList.filter(product => product.stock === true)
+    displayProducts(filter);
+}
+
+function filterOutStock() {
+    let filter = productsList.filter(product => product.stock === false)
+    displayProducts(filter);
+}
 function displayCategorie(category) {
-    let filtedcategories = products.filter(product => product.category === category);
+    let filtedcategories = productsList.filter(product => product.category === category);
 
     productContainer.innerHTML = ''
     filtedcategories.forEach((filtedcategories) => {
@@ -271,9 +202,9 @@ function displayCategorie(category) {
             </div>
         `;
         productContainer.appendChild(productCard);
-    });
-};
 
+    });
+    
 
 }
 
@@ -284,24 +215,104 @@ const dropdownButton = document.getElementById("dropdownButton");
 const dropdownMenu = document.getElementById("dropdownMenu");
 const dropdownIcon = document.getElementById("dropdownIcon");
 
+function compteur() {
+    const affichage = document.getElementById('cmpt');
+    console.log(affichage)
+    let cmp = JSON.parse(localStorage.getItem("addedProducts")).length;
+    affichage.textContent = cmp;
+    affichage.style.color = "white";
+}
+
 dropdownButton.addEventListener("click", function (event) {
-
     event.stopPropagation();
-
-
     dropdownMenu.classList.toggle("hidden");
-
     dropdownIcon.classList.toggle("rotate-180");
 });
 
+function searchByName() {
+    const recherche = document.getElementById('Search').value.toLowerCase();
+    const filter = productsList.filter(prd => prd.nom_product.toLowerCase().includes(recherche));
+    productContainer.innerHTML = '';
+    filter.forEach(product => {
+        const productCard = document.createElement('div');
+        productCard.innerHTML = ``
+        productCard.innerHTML = `
+             <div
+                class="relative mt-10 ml-2 mr-4 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
+                <a class="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl" href="#">
+                    <div class="w-full h-full" style="background-color: #d6d5d1;">
+                        <img class="object-cover h-full w-full" src="${product.image}" alt="product image" />
+                    </div>
+                    <span class="absolute top-0 left-0 m-2 rounded-full bg-${product.sale === "Sale" ? "black" : "white"} px-2 text-center text-sm font-medium text-${product.sale === "Sale" ? "white" : "black"}">
+                        ${product.sale} <i class="fa-solid fa-fire-flame-curved"></i>
+                    </span>
+                    <span class="absolute top-0 right-0 m-2 rounded-full px-2 text-center text-sm font-medium bg-white">
+                        <i class="fa-regular fa-heart bg-${product.favorite === "true"? "#dc2626" : "#d6d3d1"}"></i>
+                    </span>
+                </a>
+                <div class="mt-4 px-5 pb-5">
+                    <a href="#">
+                        <h5 class="text-xl tracking-tight text-slate-900">${product.nom_product}</h5>
+                    </a>
+                    <div class="mt-2 mb-5 flex items-center justify-between">
+                        <p>
+                            <span class="text-3xl font-bold text-slate-900">${product.prixNew}</span>
+                            <span class="text-sm text-slate-900 line-through">${product.prix}</span>
+                        </p>
+                        <div class="flex items-center">
+                            <svg aria-hidden="true" class="h-5 w-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                                </path>
+                            </svg>
+                            <svg aria-hidden="true" class="h-5 w-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                                </path>
+                            </svg>
+                            <svg aria-hidden="true" class="h-5 w-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                                </path>
+                            </svg>
+                            <svg aria-hidden="true" class="h-5 w-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                                </path>
+                            </svg>
+                            <svg aria-hidden="true" class="h-5 w-5 text-gray-200" fill="currentColor" viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                                </path>
+                            </svg>
+                            <span class="mr-2 ml-3 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold">4.0</span>
+                        </div>
+                    </div>
 
+                    <a href="#"
+                        class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300 btnAdd" onclick="add(${product.id})">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                        </svg>
+                        Add to cart</a>
+                </div>
+            </div>
+        `;
+        
 
-    function filterInStock() { 
-        let filter = products.filter(product => product.stock === true)
-        displayProducts(filter);
-    }
-    
-    function filterOutStock() {
-        let filter = products.filter(product => product.stock === false)
-        displayProducts(filter);
-    }
+        productContainer.appendChild(productCard);
+    });
+}
+
+function filterByPrice(maxPrice) {
+    filteredProducts = productsList.filter(product => product.prixNew <= maxPrice);
+    productContainer.innerHTML = '';
+    displayProducts(filteredProducts);
+}
